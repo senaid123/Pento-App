1518a26e29f74d7d3575c1c99c31431b4d635d9c
senaid123 <senaid968@gmail.com>
2022-08-13 18:50:35 +0200

    Work on HTML of survey -Need to fix bugs on demographicLive.form

--- lib/pento_web/live/user_auth_live.ex
+++ lib/pento_web/live/user_auth_live.ex
@@ -2,12 +2,11 @@ defmodule PentoWeb.UserAuthLive do
   import Phoenix.LiveView
   alias Pento.Accounts
 
-  def on_mount(_, params, %{"user_token" => user_token}, socket) do
-    user = Accounts.get_user_by_session_token(user_token)
-
+  def on_mount(_, _params, %{"user_token" => user_token} = _session, socket) do
     socket =
-      socket
-      |> assign(:current_user, user)
+      assign_new(socket, :current_user, fn ->
+        Accounts.get_user_by_session_token(user_token)
+      end)
 
     if socket.assigns.current_user do
       {:cont, socket}
